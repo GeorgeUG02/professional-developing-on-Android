fd0a5cc9037c0c231bd5e27791e57aadc228fcb6
George <georgukladnik@gmail.com>
2021-09-21 13:43:44 +0300

    lesson2-homework

--- lesson2-homework/app/src/main/java/geekbrains/ru/translator/model/data/Definition.kt
+++
@@ -1,5 +0,0 @@
-package geekbrains.ru.translator.model.data
-
-import com.google.gson.annotations.SerializedName
-
-class Definition(@field:SerializedName("definition") val definition: String?,@field:SerializedName("example") val example: String?)
--- lesson2-homework/app/src/main/java/geekbrains/ru/translator/model/data/Meanings.kt
+++
@@ -1,8 +0,0 @@
-package geekbrains.ru.translator.model.data
-
-import com.google.gson.annotations.SerializedName
-
-class Meanings(
-    @field:SerializedName("definitions") val definitions: List<Definition>?,
-    @field:SerializedName("partOfSpeech") val partofSpeech: String?
-)
